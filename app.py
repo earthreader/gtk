0629d01cfbf5d118f010c2ac94dccdd83c8a8303
Hong Minhee <minhee@dahlia.kr>
2013-08-02 03:53:58 +0900

    Use box and treeview

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import sys
 
-from gi.repository.Gtk import Application, ApplicationWindow
+from gi.repository.Gtk import (Application, ApplicationWindow, Box,
+                               CellRendererText, TreeStore, TreeView,
+                               TreeViewColumn)
 
 
 APP_ID = 'io.github.earthreader.gtk'
@@ -15,11 +17,46 @@ class EarthReaderApp(Application):
         self.connect('activate', self.on_activate)
 
     def on_activate(self, app):
-        self.window = ApplicationWindow.new(self)
-        self.window.set_title('Earth Reader')
+        self.window = ReaderWindow(application=self)
         self.window.set_default_size(600, 400)
         self.window.maximize()
         self.window.show_all()
+
+
+class ReaderWindow(ApplicationWindow):
+
+    def __init__(self, **kwargs):
+        super(ReaderWindow, self).__init__(title='Eearth Reader', **kwargs)
+        self.box = Box(spacing=0)
+        self.add(self.box)
+        feeds_store = dummy_feeds()
+        feeds_sidebar = TreeView(feeds_store)
+        cell_renderer = CellRendererText()
+        column = TreeViewColumn('Title', cell_renderer, text=0)
+        def cell_data_func(tree_view_column, renderer, model, tree_iter, *args):
+            if model[tree_iter][1] == 'category':
+                renderer.set_property('cell-background', 'silver')
+            else:
+                renderer.set_property('cell-background', None)
+        column.set_cell_data_func(cell_renderer, cell_data_func)
+        feeds_sidebar.append_column(column)
+        from gi.repository import Gtk
+        self.box.pack_start(feeds_sidebar, expand=True, fill=True, padding=0)
+        b = Gtk.Button(label='B')
+        self.box.pack_start(b, expand=True, fill=True, padding=0)
+        c = Gtk.Button(label='C')
+        self.box.pack_start(c, expand=True, fill=True, padding=0)
+
+
+def dummy_feeds():
+    store = TreeStore(str, str)
+    mine = store.append(None, ('Mine', 'category'))
+    store.append(mine, ('Romantic Binaries', 'feed'))
+    store.append(mine, ('Referentially transparent', 'feed'))
+    friends = store.append(None, ('Friends', 'category'))
+    store.append(friends, ('Null Model', 'feed'))
+    store.append(friends, ('Mearie Journal', 'feed'))
+    return store
 
 
 if __name__ == '__main__':
